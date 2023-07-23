0d84c38ffac96a6bb03f1823000a4abc49ca7702
KGB0507 <91737610+KGB0507@users.noreply.github.com>
2023-07-23 21:20:07 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,229 +7,31 @@ namespace Script
     {
         static void Main(string[] args)
         {
-            int x = 0, y = 0, z = 0;
-            bool laserNecessary = false;
-            bool firstPassage = false;
-            //int typeOfJumpers;
-            Direction direction = Direction.TOP;
-            int countRows = 0;
-            int countCryst = 0;
-            int currentCryst = 0;
             string path = args[0];
-
-            (int x, int y, bool f1, bool f2, bool f3) crystCoord;
-
 
             for (int i = 0; i < NUMOFCOLS; i++)
             {
                 Console.WriteLine("Start column");
-                crystCoord = ReadLineFromFile(countCryst, path);
-                x = crystCoord.x;
-                y = crystCoord.y;
-                Delay(0.3);
-                DateTime dateTime1 = DateTime.Now;
-                //going to the start position
-                GoTo(x, y -H1 - ALLOWEDH - H2);
-                y -= H1 + ALLOWEDH + H2;
-                GoTo(x + DX1, y);
-                x += DX1;
-                direction = Direction.TOP;
-                countRows = 0;
-                //typeOfJumpers = ReadJumpers();
-                /*switch (typeOfJumpers)
-                {
-                    case 0:
-                        {
-                            dx1 = 76;
-                            dy1 = 10;
-                            dx2 = 115;
-                            dy2 = -10;
-                            break;
-                        }
-                    case 1:
-                        {
-                            dx1 = 76;
-                            dy1 = 10;
-                            dx2 = 230;
-                            dy2 = -10;
-                            break;
-                        }
-                    case 2:
-                        {
-                            dx1 = 115;
-                            dy1 = 10;
-                            dx2 = 115;
-                            dy2 = -10;
-                            break;
-                        }
-                    case 3:
-                        {
-                            dx1 = 76;
-                            dy1 = 0;
-                            dx2 = 0;
-                            dy2 = 0;
-                            break;
-                        }
-                    case 4:
-                        {
-                            dx1 = 0;
-                            dy1 = 0;
-                            dx2 = 115;
-                            dy2 = 0;
-                            break;
-                        }
-                    case 5:
-                        {
-                            dx1 = 0;
-                            dy1 = 0;
-                            dx2 = 115;
-                            dy2 = 0;
-                            break;
-                        }
-                    case 6:
-                        {
-                            dx1 = 76;
-                            dy1 = 0;
-                            dx2 = 0;
-                            dy2 = 0;
-                            break;
-                        }
-                    default:
-                        break;
-                }*/
-
-                firstPassage = true;
-                while (countRows != 3)
-                {
-                    switch (direction)
-                    {
-                        case Direction.TOP:
-                            {
-                                while (y != HBORDER2)
-                                {
-                                    if (firstPassage)
-                                        countCryst++;
-                                    //currentCryst++;
-                                    crystCoord = ReadLineFromFile(currentCryst, path);
-                                    currentCryst++;
-                                    switch (countRows)
-                                    {
-                                        case 0:
-                                            {
-                                                if (crystCoord.f1)
-                                                    laserNecessary = true;
-                                                break;
-                                            }
-                                        case 1:
-                                            {
-                                                if (crystCoord.f2)
-                                                    laserNecessary = true;
-                                                break;
-                                            }
-                                        case 2:
-                                            {
-                                                if (crystCoord.f3)
-                                                    laserNecessary = true;
-                                                break;
-                                            }
-                                    }
-                                    FromDownToTop(x, ref y, laserNecessary);
-                                    laserNecessary = false;
-                                    //y += H1 + ALLOWEDH + H2;
-                                    if (y != HBORDER2)
-                                    {
-                                        GoTo(x, y + DISTBETWCRYST);
-                                        y += DISTBETWCRYST;
-                                    }
-                                    else
-                                    {
-                                        countRows++;
-                                        if (countRows == 1)
-                                            firstPassage = false;
-                                        if (countRows != 3)
-                                        {
-                                            GoTo(x + DX2, y);
-                                            x += DX2;
-                                        }
-                                        direction = Direction.DOWN;
-                                        break;
-                                    }
-                                }
-                                break;
-                            }
-                        case Direction.DOWN:
-                            {
-                                while (y != HBORDER1)
-                                {
-                                    currentCryst--;
-                                    crystCoord = ReadLineFromFile(currentCryst, path);
-                                    //x = crystCoord.x;
-                                    //y = crystCoord.y;
-                                    switch(countRows)
-                                    {
-                                        case 0:
-                                            {
-                                                if (crystCoord.f1)
-                                                    laserNecessary = true;
-                                                break;
-                                            }
-                                        case 1:
-                                            {
-                                                if (crystCoord.f2)
-                                                    laserNecessary = true;
-                                                break;
-                                            }
-                                        case 2:
-                                            {
-                                                if (crystCoord.f3)
-                                                    laserNecessary = true;
-                                                break;
-                                            }
-                                    }
-                                    FromTopToDown(x, ref y, laserNecessary);
-                                    //y -= H1 + ALLOWEDH + H2;
-                                    laserNecessary = false;
-                                    if (y != HBORDER1)
-                                    {
-                                        GoTo(x, y - DISTBETWCRYST);
-                                        y -= DISTBETWCRYST;
-                                    }
-                                    else
-                                    {
-                                        countRows++;
-                                        GoTo(x + DX2, y);
-                                        x += DX2;
-                                        direction = Direction.TOP;
-                                        break;
-                                    }
-                                }
-                                break;
-                            }
-                    }
-                }
+                ColumnProcessing(path);
                 Console.WriteLine("Finish column");
-                DateTime dateTime2 = DateTime.Now;
-                //Console.WriteLine(dateTime2);
-                if (dateTime2.Subtract(dateTime1).TotalSeconds > 10)
-                    Autofocus();
             }
         }
 
 
         //Crystal Parameters:
-        const int NUMOFCOLS = 3;
-        const int DISTBETWCRYST = 100;
-        const int ALLOWEDH = 20;
-        const int ALLOWEDW = 20;
-        const int HBORDER1 = -H1 - ALLOWEDH - H2;
-        const int HBORDER2 = 1200;
-        const int WBORDER1 = 0;
-        const int WBORDER2 = 2900;
-        const int H1 = 150;
-        const int H2 = 30;
-        const int DX1 = 200;
-        const int DX2 = 250;
-        const int WIDTHOFCRYST = 2*DX1 + 3*DX2;
+        public static int NUMOFCOLS = 3;
+        public static int DISTBETWCRYST = 100;
+        public static int ALLOWEDH = 20;
+        public static int ALLOWEDW = 20;
+        public static int HBORDER1 = -H1 - ALLOWEDH - H2;
+        public static int HBORDER2 = 1200;
+        public static int WBORDER1 = 0;
+        public static int WBORDER2 = 2900;
+        public static int H1 = 150;
+        public static int H2 = 30;
+        public static int DX1 = 200;
+        public static int DX2 = 250;
+        public static int WIDTHOFCRYST = 2*DX1 + 3*DX2;
         //
         enum Direction
         {
@@ -537,5 +339,209 @@ namespace Script
             }
             return crystCoord;
         }
+
+        static void ColumnProcessing(string path)
+        {
+            int x = 0, y = 0, z = 0;
+            bool laserNecessary = false;
+            bool firstPassage = false;
+            //int typeOfJumpers;
+            Direction direction = Direction.TOP;
+            int countRows = 0;
+            int countCryst = 0;
+            int currentCryst = 0;
+
+            (int x, int y, bool f1, bool f2, bool f3) crystCoord;
+
+            crystCoord = ReadLineFromFile(countCryst, path);
+            x = crystCoord.x;
+            y = crystCoord.y;
+            Delay(0.3);
+            DateTime dateTime1 = DateTime.Now;
+            //going to the start position
+            GoTo(x, y - H1 - ALLOWEDH - H2);
+            y -= H1 + ALLOWEDH + H2;
+            GoTo(x + DX1, y);
+            x += DX1;
+            direction = Direction.TOP;
+            countRows = 0;
+            //typeOfJumpers = ReadJumpers();
+            /*switch (typeOfJumpers)
+            {
+                case 0:
+                    {
+                        dx1 = 76;
+                        dy1 = 10;
+                        dx2 = 115;
+                        dy2 = -10;
+                        break;
+                    }
+                case 1:
+                    {
+                        dx1 = 76;
+                        dy1 = 10;
+                        dx2 = 230;
+                        dy2 = -10;
+                        break;
+                    }
+                case 2:
+                    {
+                        dx1 = 115;
+                        dy1 = 10;
+                        dx2 = 115;
+                        dy2 = -10;
+                        break;
+                    }
+                case 3:
+                    {
+                        dx1 = 76;
+                        dy1 = 0;
+                        dx2 = 0;
+                        dy2 = 0;
+                        break;
+                    }
+                case 4:
+                    {
+                        dx1 = 0;
+                        dy1 = 0;
+                        dx2 = 115;
+                        dy2 = 0;
+                        break;
+                    }
+                case 5:
+                    {
+                        dx1 = 0;
+                        dy1 = 0;
+                        dx2 = 115;
+                        dy2 = 0;
+                        break;
+                    }
+                case 6:
+                    {
+                        dx1 = 76;
+                        dy1 = 0;
+                        dx2 = 0;
+                        dy2 = 0;
+                        break;
+                    }
+                default:
+                    break;
+            }*/
+
+            firstPassage = true;
+            while (countRows != 3)
+            {
+                switch (direction)
+                {
+                    case Direction.TOP:
+                        {
+                            while (y != HBORDER2)
+                            {
+                                if (firstPassage)
+                                    countCryst++;
+                                //currentCryst++;
+                                crystCoord = ReadLineFromFile(currentCryst, path);
+                                currentCryst++;
+                                switch (countRows)
+                                {
+                                    case 0:
+                                        {
+                                            if (crystCoord.f1)
+                                                laserNecessary = true;
+                                            break;
+                                        }
+                                    case 1:
+                                        {
+                                            if (crystCoord.f2)
+                                                laserNecessary = true;
+                                            break;
+                                        }
+                                    case 2:
+                                        {
+                                            if (crystCoord.f3)
+                                                laserNecessary = true;
+                                            break;
+                                        }
+                                }
+                                FromDownToTop(x, ref y, laserNecessary);
+                                laserNecessary = false;
+                                //y += H1 + ALLOWEDH + H2;
+                                if (y != HBORDER2)
+                                {
+                                    GoTo(x, y + DISTBETWCRYST);
+                                    y += DISTBETWCRYST;
+                                }
+                                else
+                                {
+                                    countRows++;
+                                    if (countRows == 1)
+                                        firstPassage = false;
+                                    if (countRows != 3)
+                                    {
+                                        GoTo(x + DX2, y);
+                                        x += DX2;
+                                    }
+                                    direction = Direction.DOWN;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    case Direction.DOWN:
+                        {
+                            while (y != HBORDER1)
+                            {
+                                currentCryst--;
+                                crystCoord = ReadLineFromFile(currentCryst, path);
+                                //x = crystCoord.x;
+                                //y = crystCoord.y;
+                                switch (countRows)
+                                {
+                                    case 0:
+                                        {
+                                            if (crystCoord.f1)
+                                                laserNecessary = true;
+                                            break;
+                                        }
+                                    case 1:
+                                        {
+                                            if (crystCoord.f2)
+                                                laserNecessary = true;
+                                            break;
+                                        }
+                                    case 2:
+                                        {
+                                            if (crystCoord.f3)
+                                                laserNecessary = true;
+                                            break;
+                                        }
+                                }
+                                FromTopToDown(x, ref y, laserNecessary);
+                                //y -= H1 + ALLOWEDH + H2;
+                                laserNecessary = false;
+                                if (y != HBORDER1)
+                                {
+                                    GoTo(x, y - DISTBETWCRYST);
+                                    y -= DISTBETWCRYST;
+                                }
+                                else
+                                {
+                                    countRows++;
+                                    GoTo(x + DX2, y);
+                                    x += DX2;
+                                    direction = Direction.TOP;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                }
+            }
+            DateTime dateTime2 = DateTime.Now;
+            //Console.WriteLine(dateTime2);
+            if (dateTime2.Subtract(dateTime1).TotalSeconds > 10)
+                Autofocus();
+        }
+
     }
 }
